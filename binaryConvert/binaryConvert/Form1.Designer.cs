a89937c3d2cb6ecadfd390ae250a7e3fffeaa6d3
ZarLina <6394059@gmail.com>
2017-09-15 10:52:36 +0700

    Add files via upload

---
+++ binaryConvert/binaryConvert/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace binaryConvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enter_num = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.convert = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // enter_num
+            // 
+            this.enter_num.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enter_num.Location = new System.Drawing.Point(195, 70);
+            this.enter_num.Name = "enter_num";
+            this.enter_num.Size = new System.Drawing.Size(100, 29);
+            this.enter_num.TabIndex = 0;
+            // 
+            // output
+            // 
+            this.output.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(195, 129);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(100, 29);
+            this.output.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter your number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(56, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Binary Result";
+            // 
+            // convert
+            // 
+            this.convert.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convert.Location = new System.Drawing.Point(87, 239);
+            this.convert.Name = "convert";
+            this.convert.Size = new System.Drawing.Size(90, 40);
+            this.convert.TabIndex = 4;
+            this.convert.Text = "Convert";
+            this.convert.UseVisualStyleBackColor = true;
+            this.convert.Click += new System.EventHandler(this.convert_Click);
+            // 
+            // clear
+            // 
+            this.clear.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear.Location = new System.Drawing.Point(205, 239);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(90, 40);
+            this.clear.TabIndex = 5;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 326);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.convert);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.enter_num);
+            this.Name = "Form1";
+            this.Text = "Binary Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox enter_num;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button convert;
+        private System.Windows.Forms.Button clear;
+    }
+}
+
---
+++ binaryConvert/binaryConvert/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace binaryConvert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convert_Click(object sender, EventArgs e)
+        {
+           int input = int.Parse(enter_num.Text);
+           output.Text = Convert.ToString(input, 2);
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            enter_num.Clear();
+            output.Clear();
+        }
+    }
+}
